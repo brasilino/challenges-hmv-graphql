daecea51bb392bc936cd98499081daabd0076330
Alan <alan_brasilino@icloud.com>
2022-03-31 00:42:37 -0300

    Adiciona consulta de emergencia por id

--- src/infrastructure/resolvers/queries/emergencies/index.js
+++ src/infrastructure/resolvers/queries/emergencies/index.js
@@ -9,10 +9,22 @@ const findAllByStatusEmergency = async () => {
     return result
 }
 
+const findById = async (id) => {
+    const { host } = config
+    const service = new EmergenciesService(axios, host)
+    const result = await service.findById(id)
+    return result
+}
+
     
 module.exports = {
     async findAllByStatusEmergency(_, args) {
         const result = await findAllByStatusEmergency();
+        return result
+    },
+    async findById(_, args) {
+        const { id } = args
+        const result = await findById(id);
         return result
     }
 }
--- src/infrastructure/services/emergencies/index.js
+++ src/infrastructure/services/emergencies/index.js
@@ -2,20 +2,27 @@ class EmergenciesService {
     constructor(api, host) {
         this.api = api
         this.url = host+'emergencias'
+        this.config = {
+            headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDg3NjQxMjAsInVzZXJfbmFtZSI6InRhbmdvbWFzdGVyMTAxMEBobXYtbWFzdGVyLmNvbS5iciIsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFTVEVSIl0sImp0aSI6ImY1M2ExZmNmLTg2ZTgtNDI0MS1iNmIwLWY4ZTgzODQzODA1NCIsImNsaWVudF9pZCI6Imhtdi1jbGllbnRzLXRhbmdvMTAxMCIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.mkeux072OQDw4zN_THSkyQBJuzvehX-f6wwOIvJL-ek` }
+        };
     }
 
     async findAllByStatusEmergency() {
         try {
-
-            const config = {
-                headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDg3NjQxMjAsInVzZXJfbmFtZSI6InRhbmdvbWFzdGVyMTAxMEBobXYtbWFzdGVyLmNvbS5iciIsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFTVEVSIl0sImp0aSI6ImY1M2ExZmNmLTg2ZTgtNDI0MS1iNmIwLWY4ZTgzODQzODA1NCIsImNsaWVudF9pZCI6Imhtdi1jbGllbnRzLXRhbmdvMTAxMCIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.mkeux072OQDw4zN_THSkyQBJuzvehX-f6wwOIvJL-ek` }
-            };
-
-            
-
-            const { data } = await this.api.get(this.url, config)
+            const { data } = await this.api.get(this.url, this.config)
 
             return data
+        }
+        catch(err) {
+            console.log('ERRO:', err)
+        }
+    }
+
+    async findById(id) {
+        try {
+            const { data } = await this.api.get(this.url+'/'+id, this.config)
+
+            return { content: data }
         }
         catch(err) {
             console.log('ERRO:', err)
